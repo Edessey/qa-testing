9a702276fba7bcf6f34eb970a6d99925238578b3
Edessey <edgarzbu4@gmail.com>
2020-11-15 13:53:33 +0200

    Initial commit

---
+++ src/test/java/QaFirstTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+public class QaFirstTest {
+
+    @Test//anotation
+    public void firstTest() {
+        System.out.println("Hello, corona!");
+
+      int sum = sumTwoDigits(17, 87);
+        System.out.println(sum);
+
+        System.out.println(sumTwoDigits(9,13));
+    }
+
+    private int sumTwoDigits(int a, int b) {
+        int c = a + b;
+        return c;
+    }
+
+
+}
