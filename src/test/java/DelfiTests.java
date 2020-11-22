5514f81e8088e9bf15fa380c104f50365821f365
Edessey <edgarzbu4@gmail.com>
2020-11-22 15:01:53 +0200

    Commit from second PC

--- src/test/java/DelfiTests.java
+++ src/test/java/DelfiTests.java
@@ -4,9 +4,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DelfiTests {
     @Test
+  //  annotation
     public void firstTest() {
-        System.setProperty("webdriver.chrome.driver", "D:\\Java\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "D://Java//chromedriver.exe");
+  //      String text = "";
         WebDriver driver = new ChromeDriver();
+  //      WebDriver anotherdriver = new ChromeDriver();
         driver.get("http://delfi.lv");
+  //      anotherdriver.get("http://facebook.com");
     }
 }
